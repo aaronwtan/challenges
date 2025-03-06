25bc01bcdc4250ea64e058a2c6a48255acc484c6
Aaron Tan <18561712+aaronwtan@users.noreply.github.com>
2025-03-06 16:02:19 -0600

    Add Triangles challenge

--- easy_challenges/triangles.rb
+++ easy_challenges/triangles.rb
@@ -11,4 +11,26 @@
 # and the sum of the lengths of any two sides must be greater than the length
 # of the third side.
 
+class Triangle
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
 
+    raise ArgumentError unless valid?
+  end
+
+  def kind
+    case sides.uniq.size
+    when 1 then 'equilateral'
+    when 2 then 'isosceles'
+    when 3 then 'scalene'
+    end
+  end
+
+  private
+
+  attr_reader :sides
+
+  def valid?
+    sides.all?(&:positive?) && sides.sum > sides.max * 2
+  end
+end
